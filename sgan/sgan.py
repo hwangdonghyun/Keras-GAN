993da872c04660778f1be1d081146df58a4fa259
Dong-Hyun Hwang(Laptop) <hdhpow@naver.com>
2017-12-16 22:46:04 +0900

    Initial commit

--- sgan/sgan.py
+++ sgan/sgan.py
@@ -191,7 +191,8 @@ class SGAN():
                 axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
                 axs[i,j].axis('off')
                 cnt += 1
-        fig.savefig("sgan/images/mnist_%d.png" % epoch)
+
+        fig.savefig("images/mnist_%d.png" % epoch)
         plt.close()
 
     def save_model(self):
